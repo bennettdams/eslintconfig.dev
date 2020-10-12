f3df5ea29374e2866701cfab5ff2a114d3c6e33b
Bennett <dams.bennett@gmail.com>
2020-10-12 14:43:20 +0200

    Update explanations of .eslintrc.js

--- pages/cra/typescript-prettier.tsx
+++ pages/cra/typescript-prettier.tsx
@@ -16,11 +16,11 @@ export default function PageCreateReactAppTypeScriptPrettier() {
         <CodeBlock isScrollable>
           {`module.exports = {
   extends: [
-    "react-app", // create-react-app base settings
-    "eslint:recommended", // recommended eslint rules
-    "plugin:@typescript-eslint/recommended", // recommended typescript rules
-    "prettier/@typescript-eslint", // disables typescript formatting rules
-    "plugin:prettier/recommended", // disables eslint formatting rules and uses prettier (must be last)
+    "react-app", // Create React App base settings
+    "eslint:recommended", // recommended ESLint rules
+    "plugin:@typescript-eslint/recommended", // recommended rules from @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with Prettier.
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display Prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {},
 };
