5ba76dec56cd573542c6d7da82551a85113d56d4
Danilo Cabello <cabello@users.noreply.github.com>
2022-07-25 10:11:19 -0400

    Packaged merged into prettier

--- pages/cra/typescript-prettier.tsx
+++ pages/cra/typescript-prettier.tsx
@@ -19,7 +19,6 @@ export default function PageCreateReactAppTypeScriptPrettier() {
     "react-app", // Create React App base settings
     "eslint:recommended", // recommended ESLint rules
     "plugin:@typescript-eslint/recommended", // recommended rules from @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with Prettier.
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display Prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {},
